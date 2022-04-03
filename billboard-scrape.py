db8dc84efba0f826f12a16bf2cbc2f470351b6f9
kscully-dotcom <betz@my.ccsu.edu>
2022-04-02 22:02:53 -0500

    web scraping modules created

---
+++ billboard-scrape.py
@@ -0,0 +1,23 @@
+import billboard
+import pandas as pd
+
+rows = []
+headers = ['year', 'track rank', 'track name', 'track artist']
+
+
+for year in range(1960, 2023):
+    chart = []
+    print("Scraping year {}".format(year))
+    chart = billboard.ChartData('hot-100', date="{}-12-31".format(year))
+    for i in range (0, 51):
+        cells = []
+        entry = chart[i]
+        track_title = entry.title
+        track_artist = entry.artist
+        track_rank = entry.rank
+        cells = [year] + [track_rank] + [track_title] + [track_artist]
+        rows.append(cells)
+        df = pd.DataFrame(rows, columns=headers)
+    
+
+df.to_excel('billboard-1960-2022.xlsx', index=False)
